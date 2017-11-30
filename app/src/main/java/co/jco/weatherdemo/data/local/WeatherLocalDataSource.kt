4b768fc742589e91dcd0975537f73e542fb5e716
jocollet <jhnct.dev@gmail.com>
2017-11-30 17:22:35 +0100

    Add a LocalDataSource using Realm
    Modify DataSource Interface, cities list is not returned on add/remove
    Add a switch to specify if you want to use remote or fake source (dev mode)
    Add mapping fun realm <-> local, retrofit -> local
    Add a Realm bean

---
+++ app/src/main/java/co/jco/weatherdemo/data/local/WeatherLocalDataSource.kt
@@ -0,0 +1,47 @@
+package co.jco.weatherdemo.data.local
+
+import co.jco.weatherdemo.data.*
+
+import co.jco.weatherdemo.data.local.beans.RealmWeatherCity
+import io.realm.Realm
+
+/**
+ * Local WeatherDataSource, facade to Realm database
+ * Unused at the moment
+ */
+class WeatherLocalDataSource : WeatherDataSource {
+    override fun getCities(callback: WeatherCallback<List<WeatherCity>>) {
+        val realm = Realm.getDefaultInstance()
+        val cities = realm.where(RealmWeatherCity::class.java).findAllSorted(RealmWeatherCity.CITY_NAME).map { it.fromRealmExt() }
+        realm.close()
+        callback.onResponse(cities, true)
+    }
+
+    override fun addCity(city: String, callback: WeatherCallback<WeatherCity>) {
+        throw UnsupportedOperationException("Implement me !")
+    }
+
+    override fun saveCity(city: WeatherCity, callback: WeatherCallback<WeatherCity>) {
+        val realm = Realm.getDefaultInstance()
+        realm.executeTransaction {
+            realm.copyToRealmOrUpdate(city.toRealmExt())
+        }
+        callback.onResponse(city, true)
+    }
+
+    override fun getForecast(city: String, callback: WeatherCallback<WeatherForecast>) {
+        throw UnsupportedOperationException("Implement me !")
+    }
+
+    override fun removeCity(city: String, callback: WeatherCallback<WeatherCity>) {
+        val realm = Realm.getDefaultInstance()
+        var removed: RealmWeatherCity?
+        var weatherCity: WeatherCity? = null
+        realm.executeTransaction {
+            removed = realm.where(RealmWeatherCity::class.java).equalTo(RealmWeatherCity.CITY_NAME, city).findFirst()
+            weatherCity = removed?.fromRealmExt()
+            removed?.deleteFromRealm()
+        }
+        callback.onResponse(weatherCity, weatherCity != null)
+    }
+}
