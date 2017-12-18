ff3d600d3232424260ddc7ffd338fe621d026eca
jocollet <jhnct.dev@gmail.com>
2017-12-18 08:40:40 +0100

    Remove wrong annotation on replaceFragment helper method

--- app/src/main/java/co/jco/weatherdemo/Utils.kt
+++ app/src/main/java/co/jco/weatherdemo/Utils.kt
@@ -1,12 +1,12 @@
 package co.jco.weatherdemo
 
-import android.support.annotation.LayoutRes
+import android.support.annotation.IdRes
 import android.support.v4.app.Fragment
 import android.support.v7.app.AppCompatActivity
 
 /**
  * Helper function to replace a fragment in a transation
  */
-fun AppCompatActivity.replaceFragment(@LayoutRes frameId: Int, fragment: Fragment) {
+fun AppCompatActivity.replaceFragment(@IdRes frameId: Int, fragment: Fragment) {
     //TODO WS3 replace given fragment into given frameId using fragment manager !
 }
