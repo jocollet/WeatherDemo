fe08ba61da364c29a253e67f54f3cfd0b933e88f
jocollet <jhnct.dev@gmail.com>
2018-01-16 18:06:25 +0100

    Add RX unit tests as a tutorial

---
+++ app/src/test/java/co/jco/weatherdemo/countries/CountriesServiceImpl.kt
@@ -0,0 +1,97 @@
+package co.jco.weatherdemo.countries
+
+import java.util.concurrent.FutureTask
+import java.util.concurrent.TimeUnit
+
+import io.reactivex.Observable
+import io.reactivex.Single
+import io.reactivex.schedulers.Schedulers
+
+internal class CountriesServiceImpl : CountriesService {
+
+    override fun countryNameInCapitals(country: Country): Single<String> {
+        return Single.just(country.name.toUpperCase())
+    }
+
+    override fun countCountries(countries: List<Country>): Single<Int> {
+        return Single.just(countries.size)
+    }
+
+    override fun listPopulationOfEachCountry(countries: List<Country>): Observable<Long> {
+        return Observable
+                .fromIterable(countries)
+                .map{it.population}
+    }
+
+    override fun listNameOfEachCountry(countries: List<Country>): Observable<String> {
+        return Observable
+                .fromIterable(countries)
+                .map{it.name}
+    }
+
+    override fun listOnly3rdAnd4thCountry(countries: List<Country>): Observable<Country> {
+        return Observable
+                .fromIterable(countries)
+                .skip(2)
+                .take(2)
+    }
+
+    override fun isAllCountriesPopulationMoreThanOneMillion(countries: List<Country>): Single<Boolean> {
+        return Observable
+                .fromIterable(countries)
+                .all(countryHasMoreThan1MillionPopulation())
+    }
+
+    override fun listPopulationMoreThanOneMillion(countries: List<Country>): Observable<Country> {
+        return Observable
+                .fromIterable(countries)
+                .filter(countryHasMoreThan1MillionPopulation())
+    }
+
+    override fun listPopulationMoreThanOneMillionWithTimeoutFallbackToEmpty(countriesFromNetwork: FutureTask<List<Country>>): Observable<Country> {
+        return Observable.fromFuture(countriesFromNetwork, Schedulers.io())
+                .flatMapIterable{ country -> country }
+                .filter(countryHasMoreThan1MillionPopulation())
+                .timeout(1, TimeUnit.SECONDS, Observable.empty<Country>())
+    }
+
+    override fun getCurrencyUsdIfNotFound(countryName: String, countries: List<Country>): Observable<String> {
+        return Observable
+                .fromIterable(countries)
+                .filter { it.name == countryName }
+                .map{it.currency }
+                .first("USD")
+                .toObservable()
+    }
+
+    override fun sumPopulationOfCountries(countries: List<Country>): Observable<Long> {
+        return Observable
+                .fromIterable(countries)
+                .map{ it.population }
+                .reduce { x, sum -> x + sum }
+                .toObservable()
+    }
+
+    override fun mapCountriesToNamePopulation(countries: List<Country>): Single<Map<String, Long>> {
+        return Observable
+                .fromIterable(countries)
+                .toMap({it.name}, {it.population})
+    }
+
+    override fun sumPopulationOfCountries(countryObservable1: Observable<Country>,
+                                          countryObservable2: Observable<Country>): Observable<Long> {
+        return countryObservable1
+                .concatWith(countryObservable2)
+                .map{ it.population }
+                .reduce { x, sum -> x + sum }
+                .toObservable()
+    }
+
+    override fun areEmittingSameSequences(countryObservable1: Observable<Country>,
+                                          countryObservable2: Observable<Country>): Single<Boolean> {
+        return Observable.sequenceEqual(countryObservable1, countryObservable2)
+    }
+
+    private fun countryHasMoreThan1MillionPopulation() = { country: Country -> country.population > 1_000_000 }
+
+}
---
+++ app/src/test/java/co/jco/weatherdemo/countries/Readme.md
@@ -0,0 +1,3 @@
+This package is taken from https://github.com/sergiiz/RxBasicsKata repository.
+
+Associated article : https://proandroiddev.com/practical-challenges-for-rxjava-learners-1821c454de9
